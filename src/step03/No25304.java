f28d38aa45e0503a23d601a333983dd3e086b16a
Hyerin Lee <hyerinlee0v0@gmail.com>
2023-08-28 02:52:10 +0900

    study07 commit

---
+++ src/step03/No25304.java
@@ -0,0 +1,28 @@
+package step03;
+
+import java.util.Scanner;
+
+public class No25304 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int X = sc.nextInt();
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        int sum = 0;
+
+        for(int i = 0; i < N; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            arr[i] = a * b;
+            sum += arr[i];
+        }
+
+        if(X == sum) {
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
