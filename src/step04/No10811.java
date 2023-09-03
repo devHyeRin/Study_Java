c734dcedd1281dc22575b7ea3aaa3c4304199d78
Hyerin Lee <hyerinlee0v0@gmail.com>
2023-09-04 02:36:20 +0900

    study14 commit

--- src/step04/No10811.java
+++ src/step04/No10811.java
@@ -2,6 +2,11 @@ package step04;
 
 import java.util.Scanner;
 
+/* 10811번 : 바구니 뒤집기
+* 첫째 줄에 N (1 ≤ N ≤ 100)과 M (1 ≤ M ≤ 100)이 주어진다.
+* 둘째 줄부터 M개의 줄에는 바구니의 순서를 역순으로 만드는 방법이 주어진다
+* 도현이는 입력으로 주어진 순서대로 바구니의 순서를 바꾼다.
+* */
 public class No10811 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
